eaa978060d66b775731faae1201612573a04b746
jephthe <jephthepetitfrere@gmail.com>
2025-02-12 02:25:42 -0500

    update

--- src/config/logger.js
+++ src/config/logger.js
@@ -3,9 +3,11 @@
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const app = express();
 
 // Create a write stream (in append mode) for logging
-const logStream = fs.createWriteStream(path.join(__dirname, '../../logs/access.log'), { flags: 'a' });
+const logStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' });
 
 // Define logging format
 const logger = morgan('combined', { stream: logStream });
@@ -13,11 +15,11 @@ const logger = morgan('combined', { stream: logStream });
 module.exports = logger;
 
 // Fix for 404 error on root path
-const express = require('express');
-const app = express();
-
 app.get('/', (req, res) => {
     res.send('Welcome to the MERN Comic Fight API');
 });
 
-module.exports = app;
+// Fix for chatSocket TypeError
+const { initializeSockets } = require('../sockets/chatSocket');
+
+module.exports = { app, initializeSockets };
